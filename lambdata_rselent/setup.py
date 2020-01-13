0af04c4da0ba12ce15b78854492a698f0b2b54cf
rselent <rselent@ufl.edu>
2020-01-13 13:45:29 -0500

    Update setup.py

--- lambdata_rselent/setup.py
+++ lambdata_rselent/setup.py
@@ -15,7 +15,7 @@ with open( "README.md", "r") as fh:
 		    description = "A collection of data science helper functions",
 		    long_description = LONG_DESCRIPTION,
 		    long_description_content_type= "text/markdown",
-		    url= "https://lambdaschool.com/courses/data-science",
+		    url= "https://github.com/rselent",
 		    packages= setuptools.find_packages(),
 		    python_requires= ">=3.5",
 		    install_requires= REQUIRED,
